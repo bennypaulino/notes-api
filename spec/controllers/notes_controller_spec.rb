ce80c450c37202f3a400a6c731e9d09a1b862138
bennypaulino <santiagopro71@gmail.com>
2017-05-20 10:33:38 -0400

    Adjust spec/controllers/notes_controller_spec.rb

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -7,4 +7,17 @@ RSpec.describe NotesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "notes#create action" do
+    it "should return 200 status-code" do
+      post :create, params: { note: {title: 'First', content: 'Hello'} }
+      expect(response).to be_success
+    end
+
+    it "should successfully create & save new note in the database" do
+    end
+
+    it "should return the created note in the response body" do
+    end
+  end
 end
