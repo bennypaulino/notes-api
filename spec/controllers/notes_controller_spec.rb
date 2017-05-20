d16a021f27ae46d04db7f59ea7a3b3aff801a1ba
bennypaulino <santiagopro71@gmail.com>
2017-05-20 11:27:47 -0400

    Refactor notes_controller_spec.rb

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -9,20 +9,21 @@ RSpec.describe NotesController, type: :controller do
   end
 
   describe "notes#create action" do
-    it "should return 200 status-code" do
+    before do
       post :create, params: { note: {title: 'First', content: 'Hello'} }
+    end
+
+    it "should return 200 status-code" do
       expect(response).to be_success
     end
 
     it "should successfully create & save new note in the database" do
-      post :create, params: { note: {title: 'First', content: 'Hello'} }
       note = Note.last
       expect(note.content).to eq('Hello')
       expect(note.title).to eq('First')
     end
 
     it "should return the created note in the response body" do
-      post :create, params: { note: {title: 'First', content: 'Hello'} }
       json = JSON.parse(response.body)
       expect(json['title']).to eq('First')
       expect(json['content']).to eq('Hello')
