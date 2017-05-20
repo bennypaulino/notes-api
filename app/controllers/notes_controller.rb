23fb5a21f776756abdb2e92c31eadbd2b72e9738
bennypaulino <santiagopro71@gmail.com>
2017-05-20 10:49:24 -0400

    Adjust controllers/notes_controller.rb

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,5 +3,12 @@ class NotesController < ApplicationController
   end
 
   def create
+    Note.create(note_params)
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :content)
   end
 end
