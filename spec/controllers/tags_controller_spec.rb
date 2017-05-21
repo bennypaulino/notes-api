4d07b001f6c760412af79e5ce79b551580628328
bennypaulino <santiagopro71@gmail.com>
2017-05-21 11:08:16 -0400

    Write failing test for tag w/empty string as name

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -15,4 +15,12 @@ RSpec.describe TagsController, type: :controller do
       expect(@note.tags.first.name).to eq('shiny')
     end
   end
+
+  describe "tags#create action validations" do
+    it "should properly deal with validation error" do
+      note = FactoryGirl.create(:note)
+      post :create, params: { tag: { name: '' }, note_id: note.id }
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
 end
