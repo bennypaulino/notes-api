a7fa41ed1dd86a9ef0356a2479d5e66a3143f6a5
bennypaulino <santiagopro71@gmail.com>
2017-05-21 10:03:10 -0400

    Refactor tags_controller_spec.rb

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -2,16 +2,17 @@ require 'rails_helper'
 
 RSpec.describe TagsController, type: :controller do
   describe "tags#create action" do
+    before do
+      @note = FactoryGirl.create(:note)
+      post :create, params: { tag: { name: 'shiny' }, note_id: @note.id }
+    end
+
     it "should return 200 status-code" do
-      note = FactoryGirl.create(:note)
-      post :create, params: { tag: { name: 'shiny' }, note_id: note.id }
       expect(response).to be_success
     end
 
     it "should successfully create and save a new tag in the database" do
-      note = FactoryGirl.create(:note)
-      post :create, params: { tag: { name: 'shiny' }, note_id: note.id }
-      expect(note.tags.first.name).to eq('shiny')
+      expect(@note.tags.first.name).to eq('shiny')
     end
   end
 end
