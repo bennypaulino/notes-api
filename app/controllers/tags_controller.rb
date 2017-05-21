f0712ec7876b8211e16f3fe3f25580a5d36790bc
bennypaulino <santiagopro71@gmail.com>
2017-05-21 09:58:15 -0400

    Adjust controllers/tags_controller.rb

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,12 @@
 class TagsController < ApplicationController
   def create
+    note = Note.find_by_id(params[:note_id])
+    note.tags.create(tag_params)
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
   end
 end
