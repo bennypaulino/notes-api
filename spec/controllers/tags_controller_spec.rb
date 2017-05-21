3fdf1abe6b62ba3414dd070c351579f46f23500f
bennypaulino <santiagopro71@gmail.com>
2017-05-21 09:32:33 -0400

    Write failing test for tag creation

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe TagsController, type: :controller do
-
+  describe "tags#create action" do
+    it "should return 200 status-code" do
+      note = FactoryGirl.create(:note)
+      post :create, params: { tag: { name: 'shiny' }, note_id: note.id }
+      expect(response).to be_success
+    end
+  end
 end
